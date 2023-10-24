1c3032087a7c397306704e41d5222fa6187e8af2
harbeyme24 <143273418+harbeyme24@users.noreply.github.com>
2023-10-24 22:42:39 +0800

    Update Laptop.java

--- Laptop.java
+++ Laptop.java
@@ -1,1 +1,11 @@
+package AdapterPattern;
 
+public class Laptop {
+
+    private boolean charging = false;
+
+    public void plugIn() {
+        System.out.println("Laptop is plugged in.");
+        charging = true;
+    }
+}
