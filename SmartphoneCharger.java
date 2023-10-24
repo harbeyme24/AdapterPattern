1823aa62b73224adf96988ba51834617a38a1a2a
harbeyme24 <143273418+harbeyme24@users.noreply.github.com>
2023-10-24 22:46:06 +0800

    Update SmartphoneCharger.java

--- SmartphoneCharger.java
+++ SmartphoneCharger.java
@@ -1,1 +1,12 @@
+package AdapterPattern;
 
+public class SmartphoneCharger {
+
+    private boolean charging = false;
+
+    public void plugIn() {
+        System.out.println("Smartphone Charger is plugged in.");
+        charging = true;
+    }
+
+}
