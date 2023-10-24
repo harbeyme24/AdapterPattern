f049fd5d3cbba185b17a4c2ff81547c6887f7369
harbeyme24 <143273418+harbeyme24@users.noreply.github.com>
2023-10-24 22:45:05 +0800

    Update RefrigeratorAdapter.java

--- RefrigeratorAdapter.java
+++ RefrigeratorAdapter.java
@@ -1,1 +1,14 @@
+package AdapterPattern;
 
+public class RefrigeratorAdapter implements PowerOutlet {
+    private Refrigerator ref;
+
+    public RefrigeratorAdapter(Refrigerator ref)   {
+        this.ref = ref;
+    }
+
+    @Override
+    public void plugIn() {
+        ref.startCooling();
+    }
+}
