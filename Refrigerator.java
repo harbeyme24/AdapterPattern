6d5e3576be313a26a01a82567829e2aea98732a8
harbeyme24 <143273418+harbeyme24@users.noreply.github.com>
2023-10-24 22:44:29 +0800

    Update Refrigerator.java

--- Refrigerator.java
+++ Refrigerator.java
@@ -1,1 +1,10 @@
+package AdapterPattern;
 
+public class Refrigerator {
+    private boolean cooling = false;
+
+    public void plugIn() {
+        System.out.println("Refrigerator is plugged in.");
+        cooling = true;
+    }
+}
