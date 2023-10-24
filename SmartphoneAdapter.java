2aafdf627c0b49481d15a744b83da6b5be810cfa
harbeyme24 <143273418+harbeyme24@users.noreply.github.com>
2023-10-24 22:45:25 +0800

    Update SmartphoneAdapter.java

--- SmartphoneAdapter.java
+++ SmartphoneAdapter.java
@@ -1,1 +1,14 @@
+package AdapterPattern;
 
+public class SmartphoneAdapter implements PowerOutlet {
+    private SmartphoneCharger charger;
+
+    public SmartphoneAdapter(SmartphoneCharger charger) {
+        this.charger = charger;
+    }
+
+    @Override
+    public void plugIn() {
+        charger.chargePhone();
+    }
+}
